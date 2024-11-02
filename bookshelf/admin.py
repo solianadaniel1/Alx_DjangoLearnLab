18cc8247f0d6a1b11b482c3784825228b6bf1b7f
solianadaniel1 <solianadaniel22@gmail.com>
2024-11-02 13:15:22 +0300

    Added customizations for the admin interface

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns to display
+    list_filter = ('author', 'publication_year')  # Filters to add on the sidebar
+    search_fields = ('title', 'author')  # Fields to include in the search bar
+
+admin.site.register(Book, BookAdmin)
