53c89b3fcdc20d38214475f744c80fee0ba22e2b
solianadaniel1 <solianadaniel22@gmail.com>
2024-11-09 19:48:48 +0300

    Used filter to query all books by the specific author

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query 1: All books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)
     for book in books:
         print(book.title)
 
