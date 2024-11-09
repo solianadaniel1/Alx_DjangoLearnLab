941eaf5e66c3ff926e664ba5b13f3ed3e1d695cb
solianadaniel1 <solianadaniel22@gmail.com>
2024-11-09 21:38:33 +0300

    Added custom permissions for adding, editing, and deleting books; enforced permissions in views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    path('books/', views.book_list, name='book_list'),  # Add your book list view
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),  # URL for adding a book
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),  # URL for editing a specific book
 ]
