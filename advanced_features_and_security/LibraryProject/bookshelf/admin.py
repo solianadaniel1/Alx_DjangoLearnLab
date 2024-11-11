06e3f56ab4ad18da01c8c7942d45413a2e3e1c7f
solianadaniel1 <solianadaniel22@gmail.com>
2024-11-11 10:08:07 +0300

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 from .models import Book
+from django.contrib.auth.models import Group
 
 
 class CustomUserAdmin(UserAdmin):
@@ -18,8 +19,9 @@ admin.site.register(CustomUser, CustomUserAdmin)
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to display
-    list_filter = ('author', 'publication_year')  # Filters to add on the sidebar
-    search_fields = ('title', 'author')  # Fields to include in the search bar
+    list_display = ('title', 'author', 'publication_year')  # Ensure publication_year is in the list
+    list_filter = ('publication_year',)  # Enable filtering by publication_year
 
 admin.site.register(Book, BookAdmin)
+
+
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from django.http import HttpResponseForbidden
 
-# Create your views here.
+# View for creating a book
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        description = request.POST.get('description')
+        publication_date = request.POST.get('publication_date')
+        
+        book = Book.objects.create(
+            title=title,
+            author=author,
+            description=description,
+            publication_date=publication_date
+        )
+        return render(request, 'book_detail.html', {'book': book})
+    return render(request, 'create_book.html')
+
+# View for editing a book
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.description = request.POST.get('description')
+        book.publication_date = request.POST.get('publication_date')
+        book.save()
+        return render(request, 'book_detail.html', {'book': book})
+    return render(request, 'edit_book.html', {'book': book})
+
+# View for deleting a book
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return render(request, 'book_list.html')
+
+# View for viewing a book
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'book_detail.html', {'book': book})
