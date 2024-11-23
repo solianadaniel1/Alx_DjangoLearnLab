869bf8f3f3cd25fff810b48d8235aab85144680e
solianadaniel1 <solianadaniel22@gmail.com>
2024-11-23 12:20:29 +0300

    Add token authentication and permissions to API

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+
 
 # Initialize the router
 router = DefaultRouter()
@@ -10,5 +13,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-     path('', include(router.urls)), # This will automatically generate CRUD routes for 'books_all'
+    path('', include(router.urls)), # This will automatically generate CRUD routes for 'books_all'
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # Token retrieval view
+
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from .models import Book  # Import your Book model
 from .serializers import BookSerializer  # Import your serializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Define the BookList view
 class BookList(generics.ListAPIView):  # Ensure the class extends ListAPIView
@@ -11,3 +12,14 @@ class BookList(generics.ListAPIView):  # Ensure the class extends ListAPIView
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Get all books from the database
     serializer_class = BookSerializer  # Use the BookSerializer to serialize the data
+    permission_classes = [IsAuthenticated]  # Default to IsAuthenticated for all users
+    
+    def get_permissions(self):
+        """
+        Override to allow admins to delete books
+        """
+        if self.action == 'destroy':  # Allow only admin to delete
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
