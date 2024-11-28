c239a6b1e6132f970b92ba519a1cb042e7e6e5cc
solianadaniel1 <solianadaniel22@gmail.com>
2024-11-28 09:20:36 +0300

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4&4k+qsx)5vlh=$-vp1f@$f@dc3-=0415)n=2ww^ie#0cyqot5'
+SECRET_KEY = 'django-insecure-!9jw@p$7&5iwf700ni@1%b4c-3_^=92+@k+3tzm^5vw_xr(o1n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
